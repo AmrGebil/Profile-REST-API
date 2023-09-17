771f7b74104b6eadebe836fc197c5c4b4629b244
Amr Gebil <amrgebil1@gmail.com>
2023-09-17 11:39:22 +0300

    added ViewSet api to the project with it`s functions list,create,.....

--- project/profiles_api/urls.py
+++ project/profiles_api/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
-from .views import HelloApi
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import HelloApi,HelloViewSet
+
+router=DefaultRouter()
+
+router.register('hello-viewset',HelloViewSet,basename='hello-viewset')
+
 urlpatterns = [
-   path('helloApi/',HelloApi.as_view() )
+   path('helloApi/',HelloApi.as_view() ),
+   path('',include(router.urls) )
 ]
